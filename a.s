920fa9fd5dad2146cf06264114079771453d9aaf
LePhobix <paulhans256@gmail.com>
2022-04-22 15:31:43 +0200

    Escape sequences (\n\n... doesn't work)

--- a.s
+++ a.s
@@ -14,7 +14,7 @@ pushl %ebp
 movl %esp, %ebp
 movl $4, %eax
 movl $1, %ebx
-pushl $0x065
+pushl $0x0ad65
 pushl $0x067617567
 pushl $0x06e616c20
 pushl $0x063617420
@@ -25,7 +25,7 @@ pushl $0x02c646c72
 pushl $0x06f77206f
 pushl $0x06c6c6568
 movl %esp, %ecx
-addl $74, %esp
+addl $78, %esp
 movl $40, %edx
 int $0x80
 movl $18, %eax
